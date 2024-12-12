f8f86df6755663040022a63a190f528e94050f48
Shubham Sharma <90370330+shubhamsharma-srm@users.noreply.github.com>
2024-12-13 00:16:12 +0530

    Update Combination_sum.cpp

--- Recursion/Combination_sum.cpp
+++ Recursion/Combination_sum.cpp
@@ -1,1 +1,28 @@
-I'll include the code for combination sum here.
+class Solution {
+public:
+
+    void find_combination(int index , vector<int>&arr , vector<vector<int>>&result , vector<int>&ans ,int target){
+        int n = arr.size();
+        if(index == n){
+            if(target == 0){
+                result.push_back(ans);
+            }
+            return;
+        }
+        if(arr[index]<=target){
+            ans.push_back(arr[index]);
+            find_combination(index , arr , result , ans , target - arr[index]);
+            ans.pop_back();
+        }
+        find_combination(index + 1, arr , result , ans , target);
+
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>ans;
+        vector<vector<int>>result;
+        find_combination(0,candidates,result,ans,target);
+        return result;
+        
+    }
+};
