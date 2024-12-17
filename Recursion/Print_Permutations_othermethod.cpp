c9c51f1e9e07dfae88466980d4f1f31d7c7b9867
Shubham Sharma <90370330+shubhamsharma-srm@users.noreply.github.com>
2024-12-18 00:01:16 +0530

    Update Print_Permutations_othermethod.cpp

--- Recursion/Print_Permutations_othermethod.cpp
+++ Recursion/Print_Permutations_othermethod.cpp
@@ -1,1 +1,20 @@
 //Without using extra space - SWAP METHOD
+void findPermutations(int index ,vector<int>&arr , vector<vector<int>>&result){
+    if(index == arr.size()){
+        result.push_back(arr);
+        return;
+    }
+
+    for(int i=index ;i<arr.size();i++){
+        swap(arr[index],arr[i]);
+        findPermutations(index + 1,arr,result);
+        swap(arr[index] ,arr[i]);
+
+    }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>>result;
+    findPermutations(0,nums,result);
+    return result;
+}
